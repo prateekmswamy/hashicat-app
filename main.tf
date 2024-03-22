73db7fbf76076dddd1c4733de1eee6d27dde5003
prateekmswamy <prateekmswamy@gmail.com>
2024-03-22 16:23:58 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -200,9 +200,11 @@ resource "null_resource" "configure-cat-app" {
       host     = azurerm_public_ip.catapp-pip.fqdn
     }
   }
-  module "azure-backup" {
-    source              = "ravensorb/azure-backup/azurerm"
-    version             = "1.0.2"
-    resource_group_name = "${var.prefix}-public"
-  }
+
+}
+
+module "azure-backup" {
+  source              = "ravensorb/azure-backup/azurerm"
+  version             = "1.0.2"
+  resource_group_name = "${var.prefix}-public"
 }
